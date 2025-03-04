c0ee335e8bd9f54bcfdc2d338e42b79d7821fc5f
minhquan7323 <minhquany9@gmail.com>
2025-03-04 14:08:08 +0700

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: './index.html',
-    }
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Tách các thư viện bên thứ 3
+          }
+        },
+      },
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, // Loại bỏ console.log
+        drop_debugger: true, // Loại bỏ debugger
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Tăng giới hạn cảnh báo chunk
   },
-})
+});
